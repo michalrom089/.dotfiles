e7021e1683a33b7f40e9a5a3eb12d67bf79f5cb2
Michal Romanowski <michal.romanowski@allegropay.pl>
2022-02-13 18:33:32 +0100

    add profile

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+STOW_FOLDERS="nvim,tmux,zsh"
+DOTFILES=$HOME/.dotfiles
+
+pushd $DOTFILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    stow -D $folder
+    stow $folder
+done
+popd
+
