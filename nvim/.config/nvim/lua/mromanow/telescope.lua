f7ec0e1066de4fe9dbc981b6f1d1e5f6014f731b
Michal Romanowski <michal.romanowski@allegropay.pl>
2022-02-13 18:14:22 +0100

    update telescope

--- nvim/.config/nvim/lua/mromanow/telescope.lua
+++ nvim/.config/nvim/lua/mromanow/telescope.lua
@@ -40,7 +40,7 @@ local M = {}
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
         prompt_title = "< VimRC >",
-        cwd = "~/.config/nvim",
+        cwd = vim.env.DOTFILES,
         hidden = true,
     })
 end
